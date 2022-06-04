96816a399a880faafa8b7b62c0f65a750b7d4451
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-06-04 23:26:57 +0300

    Add try/except in create_commendation

--- script.py
+++ script.py
@@ -33,24 +33,29 @@ def remove_chastisements(schoolkid):
 
 
 def create_commendation(schoolkid, subject_title):
-    schoolkid_lessons = Lesson.objects.filter(year_of_study=6, group_letter="А",subject__title=subject_title)
-    schoolkid_lesson = random.choice(schoolkid_lessons)
+    
     commendations = ["Молодец!", "Отлично!", "Хорошо!",
-    "Гораздо лучше, чем я ожидал!",
-    "Ты меня приятно удивил!", "Великолепно!", "Прекрасно!",
-    "Ты меня очень обрадовал!",
-    "Именно этого я давно ждал от тебя!"
-    "Сказано здорово – просто и ясно!",
-    "Ты, как всегда, точен!",
-    "Очень хороший ответ!", "Талантливо",
-    "Ты сегодня прыгнул выше головы!", "Я поражен!",
-    "Уже существенно лучше!", "Потрясающе!",
-    "Замечательно!", "Прекрасное начало!", "Так держать!",
-    "Ты на верном пути!", "Здорово!",
-    "Это как раз то, что нужно!", "Я тобой горжусь!",
-    "С каждым разом у тебя получается всё лучше!",
-    "Мы с тобой не зря поработали!",
-    "Я вижу, как ты стараешься!", "Ты растешь над собой!",
-    "Ты многое сделал, я это вижу!",
-    "Теперь у тебя точно все получится!"]
-    Commendation.objects.create(text=random.choice(commendations), created=schoolkid_lesson.date, schoolkid=schoolkid, subject=schoolkid_lesson.subject, teacher=schoolkid_lesson.teacher)
+        "Гораздо лучше, чем я ожидал!",
+        "Ты меня приятно удивил!", "Великолепно!", "Прекрасно!",
+        "Ты меня очень обрадовал!",
+        "Именно этого я давно ждал от тебя!"
+        "Сказано здорово – просто и ясно!",
+        "Ты, как всегда, точен!",
+        "Очень хороший ответ!", "Талантливо",
+        "Ты сегодня прыгнул выше головы!", "Я поражен!",
+        "Уже существенно лучше!", "Потрясающе!",
+        "Замечательно!", "Прекрасное начало!", "Так держать!",
+        "Ты на верном пути!", "Здорово!",
+        "Это как раз то, что нужно!", "Я тобой горжусь!",
+        "С каждым разом у тебя получается всё лучше!",
+        "Мы с тобой не зря поработали!",
+        "Я вижу, как ты стараешься!", "Ты растешь над собой!",
+        "Ты многое сделал, я это вижу!",
+        "Теперь у тебя точно все получится!"]
+    
+    try:
+        schoolkid_lessons = Lesson.objects.filter(year_of_study=6, group_letter="А",subject__title=subject_title)
+        schoolkid_lesson = random.choice(schoolkid_lessons)
+        Commendation.objects.create(text=random.choice(commendations), created=schoolkid_lesson.date, schoolkid=schoolkid, subject=schoolkid_lesson.subject, teacher=schoolkid_lesson.teacher)
+    except IndexError:
+        print("Проверьте правильность написания предмета")
