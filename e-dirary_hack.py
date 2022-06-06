ab1b2a07bcdde1baa05f7766428c26b1206652fd
Mark Tereshchenko <83189636+YungIshimura@users.noreply.github.com>
2022-06-06 22:54:44 +0300

    Linting code

--- e-dirary_hack.py
+++ e-dirary_hack.py
@@ -5,9 +5,9 @@ import random
 def get_schoolkid(schoolkid_name):
     try:
         schoolkid = Schoolkid.objects.get(full_name__contains=schoolkid_name)
-        
+
         return schoolkid
-    
+
     except Schoolkid.DoesNotExist:
         print("Такого ученика нет в базе \nПроверьте правильность написания фамилии и имени, а также их порядок \n(сначала фамилия, потом имя)")
 
@@ -16,8 +16,9 @@ def get_schoolkid(schoolkid_name):
 
 
 def fix_marks(schoolkid):
-    schoolkid_bad_marks = Mark.objects.filter(points__in=[2, 3], schoolkid=schoolkid)
-    schoolkid_bad_marks.update(points = 5)
+    schoolkid_bad_marks = Mark.objects.filter(points__in=[2, 3],
+                                              schoolkid=schoolkid)
+    schoolkid_bad_marks.update(points=5)
 
 
 def remove_chastisements(schoolkid):
@@ -26,29 +27,34 @@ def remove_chastisements(schoolkid):
 
 
 def create_commendation(schoolkid, subject_title):
-    
     commendations = ["Молодец!", "Отлично!", "Хорошо!",
-        "Гораздо лучше, чем я ожидал!",
-        "Ты меня приятно удивил!", "Великолепно!", "Прекрасно!",
-        "Ты меня очень обрадовал!",
-        "Именно этого я давно ждал от тебя!"
-        "Сказано здорово – просто и ясно!",
-        "Ты, как всегда, точен!",
-        "Очень хороший ответ!", "Талантливо",
-        "Ты сегодня прыгнул выше головы!", "Я поражен!",
-        "Уже существенно лучше!", "Потрясающе!",
-        "Замечательно!", "Прекрасное начало!", "Так держать!",
-        "Ты на верном пути!", "Здорово!",
-        "Это как раз то, что нужно!", "Я тобой горжусь!",
-        "С каждым разом у тебя получается всё лучше!",
-        "Мы с тобой не зря поработали!",
-        "Я вижу, как ты стараешься!", "Ты растешь над собой!",
-        "Ты многое сделал, я это вижу!",
-        "Теперь у тебя точно все получится!"]
-    
+                     "Гораздо лучше, чем я ожидал!",
+                     "Ты меня приятно удивил!", "Великолепно!", "Прекрасно!",
+                     "Ты меня очень обрадовал!",
+                     "Именно этого я давно ждал от тебя!"
+                     "Сказано здорово – просто и ясно!",
+                     "Ты, как всегда, точен!",
+                     "Очень хороший ответ!", "Талантливо",
+                     "Ты сегодня прыгнул выше головы!", "Я поражен!",
+                     "Уже существенно лучше!", "Потрясающе!",
+                     "Замечательно!", "Прекрасное начало!", "Так держать!",
+                     "Ты на верном пути!", "Здорово!",
+                     "Это как раз то, что нужно!", "Я тобой горжусь!",
+                     "С каждым разом у тебя получается всё лучше!",
+                     "Мы с тобой не зря поработали!",
+                     "Я вижу, как ты стараешься!", "Ты растешь над собой!",
+                     "Ты многое сделал, я это вижу!",
+                     "Теперь у тебя точно все получится!"]
+
     try:
-        schoolkid_lessons = Lesson.objects.filter(year_of_study=6, group_letter="А",subject__title=subject_title)
+        schoolkid_lessons = Lesson.objects.filter(year_of_study=6,
+                                                  group_letter="А",
+                                                  subject__title=subject_title)
         schoolkid_lesson = random.choice(schoolkid_lessons)
-        Commendation.objects.create(text=random.choice(commendations), created=schoolkid_lesson.date, schoolkid=schoolkid, subject=schoolkid_lesson.subject, teacher=schoolkid_lesson.teacher)
+        Commendation.objects.create(text=random.choice(commendations),
+                                    created=schoolkid_lesson.date,
+                                    schoolkid=schoolkid,
+                                    subject=schoolkid_lesson.subject,
+                                    teacher=schoolkid_lesson.teacher)
     except IndexError:
         print("Проверьте правильность написания предмета")
